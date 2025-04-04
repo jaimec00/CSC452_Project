ccd3bc52fb5675acedc210e97a400790c862784a
jaimec00 <hejaca00@gmail.com>
2025-04-04 12:27:57 -0400

    add download of annotations

--- data/get_data.py
+++ data/get_data.py
@@ -4,8 +4,9 @@ import argparse
 import zipfile
 import io
 from tqdm import tqdm
+import os
 
-def get_livecell_urls(args):
+def get_livecell_urls(url_file):
 
     '''
     gets the urls contiaining the live cell data.
@@ -39,11 +40,37 @@ def get_livecell_urls(args):
     # If you need a single string (like the content of urls.txt) with each URL on a new line:
     urls_text = "\n".join(urls)
 
-    with open(args.out_file, "w") as f:
+    with open(url_file, "w") as f:
         f.write(urls_text)
 
+def get_livecell_annotations(annotations_dir):
+    urls = {"training": "http://livecell-dataset.s3.eu-central-1.amazonaws.com/LIVECell_dataset_2021/annotations/LIVECell/livecell_coco_test.json",
+            "validation": "http://livecell-dataset.s3.eu-central-1.amazonaws.com/LIVECell_dataset_2021/annotations/LIVECell/livecell_coco_train.json",
+            "testing": "http://livecell-dataset.s3.eu-central-1.amazonaws.com/LIVECell_dataset_2021/annotations/LIVECell/livecell_coco_val.json"
+            }
 
-def get_livecell_images(args):
+    os.makedirs(annotations_dir, exist_ok=True)
+
+    for dataset, url in urls.items():
+        # Send a GET request with streaming enabled
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raise an error for bad status codes
+
+        # Get the total file size from the headers
+        total_size = int(response.headers.get('content-length', 0))
+        chunk_size = 1024  # Define the chunk size (in bytes)
+
+        # Open a file to write the downloaded content
+        with open(os.path.join(annotations_dir, dataset + ".json"), "wb") as file:
+            # Set up tqdm progress bar with total file size
+            with tqdm(total=total_size, unit='B', unit_scale=True, desc=f"Downloading {dataset} annotations") as progress_bar:
+                # Read and write the file in chunks
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    if chunk:  # Filter out keep-alive chunks
+                        file.write(chunk)
+                        progress_bar.update(len(chunk))
+
+def get_livecell_images(img_dir):
     # URL of the zip file to download
     zip_url = "http://livecell-dataset.s3.eu-central-1.amazonaws.com/LIVECell_dataset_2021/images_per_celltype.zip"
 
@@ -75,7 +102,7 @@ def get_livecell_images(args):
         # Set up the progress bar for the extraction process
         with tqdm(total=len(file_list), desc="Extracting") as extract_bar:
             for file_info in file_list:
-                zip_ref.extract(file_info, path=args.out_dir)
+                zip_ref.extract(file_info, path=img_dir)
                 extract_bar.update(1)
 
 
@@ -83,13 +110,18 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--out_file", type=str, default="urls.txt")
-    parser.add_argument("--out_dir", type=str, default="images")
-    parser.add_argument("--get_images", type=int, default=1)
+    parser.add_argument("--url_file", type=str, default="urls.txt")
+    parser.add_argument("--img_dir", type=str, default="images")
+    parser.add_argument("--annotations_dir", type=str, default="annotations")
+    parser.add_argument("--get_images", type=int, default=0)
+    parser.add_argument("--get_annotations", type=int, default=1)
+    parser.add_argument("--get_urls", type=int, default=0)
 
     args = parser.parse_args()
 
     if args.get_images:
-        get_livecell_images(args)
-    else:
-        get_livecell_urls(args)
+        get_livecell_images(args.img_dir)
+    if args.get_annotations:
+        get_livecell_annotations(args.annotations_dir)
+    if args.get_urls:
+        get_livecell_urls(args.url_file)
