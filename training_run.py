27020b2af60d128c0623707f7d55c0f7d3449df8
jaimec00 <hejaca00@gmail.com>
2025-04-29 12:09:54 -0400

    config for num smaples to load

--- training_run.py
+++ training_run.py
@@ -10,12 +10,9 @@ from pathlib import Path
 
 class TrainingRun():
     def __init__(self, config):
-        self.data = DataLoader(config.data.train_val_path, config.data.test_path, config.data.annotations_path)
+        self.data = DataLoader(config.data.train_val_path, config.data.test_path, config.data.annotations_path, config.data.num_train, config.data.num_val, config.data.num_test)
         self.epochs = config.training_params.epochs
         self.lr = config.training_params.lr
-
-        
-
 
 if __name__ == "__main__":
 
